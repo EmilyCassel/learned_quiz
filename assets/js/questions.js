fa62847310def8931496bfb2d3cd73010eb6852f
Emily  Cassel <emily@Emilys-MacBook-Pro.local>
2024-01-15 17:04:47 -0500

    questions finished

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,15 +1,35 @@
 var questions [
     {
         questionWords: "Fill in the blank: A block of reusable code that performs a specific task or calculates a value is a_____?",
-        choices: ["JavaScript", "Function", "Variable", "Boolean"],
+        choices: ["JavaScript", "Function", "Variable", "Boolean", "All of the Above"],
         questionAnswer: "Function"
     },
    
+    {
+        questionWords: "Which is a not a primative value?",
+        choices: ["boolean", "number", "string", "undefined","All of the Above are Primitive Values"],
+        questionAnswer: "All of the Above are Primitive Values"
+    },
 
     {
-        questionWords: "?",
-        choices: ["", "", "", ""],
-        questionAnswer: ""
+        questionWords: "Which shows up in the console when a variable has been declared buthas not been assigned a value?",
+        choices: ["null", "not defined", "undefined", "NaN","All of the Above"],
+        questionAnswer: "undefined"
     },
+
+    {
+        questionWords: "A unique and immutabale value that may be used as the key of an object property?",
+        choices: ["symbol", "number", "string", "boolean","All of the Above"],
+        questionAnswer: "symbol",
+    },
+
+    {
+        questionWords: "What is the difference between == and === in JavaScript?",
+        choices: ["== performs type coercion, while === does not", "== compares the values only, while === compares both the values and the types", " == compares both the values and the types, while === compares the values only", "== and === are interchangeable and have the same behavior","All of the Above"],
+        questionAnswer: "== performs type coercion, while === does not",
+    },
+
+
+
 
 ]
