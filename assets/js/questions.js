4a3ad26a490ccfecf5bf97d8d13ede63067740d0
Emily  Cassel <emily@Emilys-MacBook-Pro.local>
2024-01-15 15:05:02 -0500

    question1

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+var questions [
+    {
+        questionWords: "Fill in the blank: A block of reusable code that performs a specific task or calculates a value is a_____?",
+        choices: ["JavaScript", "Function", "Variable", "Boolean"],
+        questionAnswer: "Function"
+    },
+   
+
+    {
+        questionWords: "?",
+        choices: ["", "", "", ""],
+        questionAnswer: ""
+    },
+
+]
